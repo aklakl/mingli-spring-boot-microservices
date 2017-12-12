1b7d8b50bdef6ed94eab35987d487d3416b103ec
Ming <mingli19850915@gmail.com>
2017-12-12 10:27:23 -0500

    this is only for WebSecurity , we recommend using ResourceServerConfigurerAdapter to implement remove access permissions of the swagger in spring security OAuth2 ,using configuration

--- mingli-spring-boot-microservices/src/main/java/com/mingli/config/WebSecurityConfiguration.java
+++ mingli-spring-boot-microservices/src/main/java/com/mingli/config/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.mingli.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 
 
@@ -10,7 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	/**
-	 * if you want to except the swagger UI in spring security OAuth2 ,please using this configuration  
+	 * @author: MingLi
+	 * @Date: 12/11/2017
+	 * @Description: if you want to except the swagger UI in spring security OAuth2 ,please using this configuration  
+	 * @warning this is only for WebSecurity , we recommend using ResourceServerConfigurerAdapter to implement it. 
 	 */
     @Override
     public void configure(WebSecurity web) throws Exception {
